a271bc7fb3f3b20698aab628dfbfc9a9353468bd
redass12 <redataraoui1@gmail.com>
2020-06-24 01:02:33 +0200

    Updates

---
+++ precache-manifest.452acbabc6490061087a7f72fd1ea63b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23e32428385aa8f592d1858839be455",
+    "url": "/React-in-github-demo/index.html"
+  },
+  {
+    "revision": "37657710990fcada21e4",
+    "url": "/React-in-github-demo/static/css/main.602cf69d.chunk.css"
+  },
+  {
+    "revision": "03f13709f0cf443e1c6c",
+    "url": "/React-in-github-demo/static/js/2.3eed2531.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-demo/static/js/2.3eed2531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37657710990fcada21e4",
+    "url": "/React-in-github-demo/static/js/main.2a682112.chunk.js"
+  },
+  {
+    "revision": "d17e046549cea8f15b37",
+    "url": "/React-in-github-demo/static/js/runtime-main.d48c0aae.js"
+  }
+]);
